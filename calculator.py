5697b911bd4d7893c6cc251348cbec304b776f05
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-05 16:18:10 -0700

    Added default paramenter to square and cube functions

--- calculator.py
+++ calculator.py
@@ -8,13 +8,33 @@ from arithmetic import *
 
 
 # Your code goes here
-new_input = ""
-while new_input != "q":
+while True:
     new_input = raw_input("> ")
+
+    if new_input == "q":
+        break
+
     tokens = new_input.split(" ")
     operator = tokens[0]
     first_number = int(tokens[1])
     sec_number = int(tokens[2])
-    # LATER: check to make sure user input valid
-    if tokens[0] == "+":
+
+    if operator == "+":
         print add(first_number, sec_number)
+    elif operator == "-":
+        print subtract(first_number, sec_number)
+    elif operator == "*":
+        print multiply(first_number, sec_number)
+    elif operator == "/":
+        print divide(first_number, sec_number)
+    elif operator == "square":
+        print square(first_number, sec_number)
+    elif operator == "cube":
+        print cube(first_number, sec_number)
+    elif operator == "pow":
+        print power(first_number, sec_number)
+    elif operator == "mod":
+        print mod(first_number, sec_number)
+    else:
+        print "Invalid entry, please try again."
+
