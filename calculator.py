5e5e1d37c2d24cd66e3d4b88a93faa386e674bcd
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-05 17:21:20 -0700

    Added in try/except in case user enters incorrectly

--- calculator.py
+++ calculator.py
@@ -12,24 +12,26 @@ while True:
     new_input = raw_input("> ")
     if new_input == "q":
         break
-    tokens = new_input.split(" ")
-    operator = tokens[0]
-    num_list = tokens[1:len(tokens)]
-    if operator == "+":
-        print add(num_list)
-    elif operator == "-":
-        print subtract(num_list)
-    elif operator == "*":
-        print multiply(num_list)
-    elif operator == "/":
-        print divide(num_list)
-    elif operator == "square":
-        print square(num_list)
-    elif operator == "cube":
-        print cube(num_list)
-    elif operator == "pow":
-        print power(num_list)
-    elif operator == "mod":
-        print mod(num_list)
-    else:
+
+    try:
+        tokens = new_input.split(" ")
+        operator = tokens[0]
+        num_list = tokens[1:len(tokens)]
+        if operator == "+":
+            print add(num_list)
+        elif operator == "-":
+            print subtract(num_list)
+        elif operator == "*":
+            print multiply(num_list)
+        elif operator == "/":
+            print divide(num_list)
+        elif operator == "square":
+            print square(num_list)
+        elif operator == "cube":
+            print cube(num_list)
+        elif operator == "pow":
+            print power(num_list)
+        elif operator == "mod":
+            print mod(num_list)
+    except:
         print "Invalid entry, please try again."
