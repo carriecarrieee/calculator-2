d34d1a2ce839c0c08dfe5da37527392c4b4ac3b5
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-05 17:32:33 -0700

    Fixed those decimal places!

--- calculator.py
+++ calculator.py
@@ -18,21 +18,21 @@ while True:
         operator = tokens[0]
         num_list = tokens[1:len(tokens)]
         if operator == "+":
-            print add(num_list)
+            print "%.2f" % add(num_list)
         elif operator == "-":
-            print subtract(num_list)
+            print "%.2f" % subtract(num_list)
         elif operator == "*":
-            print multiply(num_list)
+            print "%.2f" % multiply(num_list)
         elif operator == "/":
-            print divide(num_list)
+            print "%.2f" % divide(num_list)
         elif operator == "square":
-            print square(num_list)
+            print "%.2f" % square(num_list)
         elif operator == "cube":
-            print cube(num_list)
+            print "%.2f" % cube(num_list)
         elif operator == "pow":
-            print power(num_list)
+            print "%.2f" % power(num_list)
         elif operator == "mod":
-            print mod(num_list)
+            print "%.2f" % mod(num_list)
         else:
             print "Invalid entry, please try again."
     except:
